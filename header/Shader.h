15175a85e977552fe528a1536ca3096bc4b75b13
Andreas <adde.engberg@gmail.com>
2021-11-11 14:28:15 +0100

    Create shader class, add initial triangle

---
+++ header/Shader.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h> // include glad to get all the required OpenGL headers
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexPath, const char* fragmentPath);
+
+	/// <summary>
+	/// Activate the shader
+	/// </summary>
+	void use()
+	{
+		glUseProgram(ID);
+	}
+
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <glm/glm.hpp>
 #include <glad/glad.h> //must be included before glfw 
 #include <GLFW/glfw3.h>
+#include "header/Shader.h"
 
 void initialize();
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -12,7 +13,7 @@ void processInput(GLFWwindow* window);
 const unsigned int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
 
 int main() {
-
+    
     initialize();
     //create window
     GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Procedural Terrain", NULL, NULL);
@@ -35,15 +36,40 @@ int main() {
 
     //Callback functions
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    Shader myShader{ "shaders/vertex.vert", "shaders/fragment.frag" };
+
+    // Create test triangle
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    //Create vertex buffer object
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //bind
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //add data, GL_DYNAMIC_DRAW if data is changed alot  
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
 
-    //render loop
+    // Render loop
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        myShader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
